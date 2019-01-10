460164204b9a8f1d4866a86284890647705497e0
yamamoto-yosuke <yamamoto-yosuke@gm.janome.co.jp>
2019-01-10 09:48:22 +0900

    1st commit

---
+++ fname2timestamp/FileSizeConverter .cs
@@ -0,0 +1,31 @@
+﻿using System.Windows.Data;
+
+namespace BindingTest.Converters
+{
+  　class FileSizeConverter : IValueConverter
+    {
+        static readonly string[] Suffix = new string[] { "", "K", "M", "G", "T" };
+        static readonly double Unit = 1024;
+
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (!(value is long)) throw new ArgumentException("long型じゃないよ。", "value");
+            double size = (double)((long)value);
+
+            int i;
+            for (i = 0; i < Suffix.Length - 1; i++)
+            {
+                if (size < Unit) break;
+                size /= Unit;
+            }
+
+            return string.Format("{0:" + (i == 0 ? "0" : "0.0") + "} {1}B", size, Suffix[i]);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+    }
+}
---
+++ fname2timestamp/ProgressBar.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace fname2timestamp
+{
+    /// <summary>
+    /// Window1.xaml の相互作用ロジック
+    /// </summary>
+    public partial class ProgresBarWindow : Window
+    {
+        public ProgresBarWindow()
+        {
+            InitializeComponent();
+        }
+        public void FileRegexProgress(int per)
+        {
+            this.progressBar.Value = per;
+            DoEvents();
+        }
+        private void Finishing(string text)
+        {
+
+        }
+        public void FileRegexCancel()
+        {
+
+        }
+        private void DoEvents()
+        {
+            DispatcherFrame frame = new DispatcherFrame();
+            var callback = new DispatcherOperationCallback(obj =>
+            {
+                ((DispatcherFrame)obj).Continue = false;
+                return null;
+            });
+            Dispatcher.CurrentDispatcher.BeginInvoke(DispatcherPriority.Background, callback, frame);
+            Dispatcher.PushFrame(frame);
+        }
+     }
+}
