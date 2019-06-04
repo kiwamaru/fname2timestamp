97fa5e304288d4c76b114c9c7e4726a306cfda16
yamamoto-yosuke <yamamoto-yosuke@gm.janome.co.jp>
2019-06-04 15:21:40 +0900

    feature:正しく選択解除イベントを取得できるように修正

--- fname2timestamp/Behaviors/BindSelectedItemsBehavior.cs
+++ fname2timestamp/Behaviors/BindSelectedItemsBehavior.cs
@@ -10,46 +10,50 @@ using System.Windows.Interactivity;
 
 namespace fname2timestamp.Behaviors
 {
-    class DataGridSelectedItemsBlendBehavior : Behavior<DataGrid>
+    /// <summary>
+    /// DataGrid内で選択されたアイテムのコレクションを保持するビヘイビア
+    /// </summary>
+    public class BindSelectedItemsBehavior : Behavior<DataGrid>
     {
-        public static readonly DependencyProperty SelectedItemsProperty =
-            DependencyProperty.Register(
-                "SelectedItems",
-                typeof(IList<object>),
-                typeof(DataGridSelectedItemsBlendBehavior),
-                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnSelectedItemsChanged)
-            );
-
+        /// <summary>
+        /// アタッチ時
+        /// </summary>
         protected override void OnAttached()
         {
             base.OnAttached();
-            AssociatedObject.SelectionChanged += OnSelectionChanged;
+
+            DataGrid grid = (DataGrid)this.AssociatedObject;
+            if (grid != null)
+            {
+                //DataGridの選択アイテム変更時のハンドラを登録
+                grid.SelectionChanged += OnSelectionChanged;
+            }
         }
 
+        /// <summary>
+        /// デタッチ時
+        /// </summary>
         protected override void OnDetaching()
         {
+            DataGrid grid = (DataGrid)this.AssociatedObject;
+            if (grid != null)
+            {
+                //DataGridの選択アイテム変更時のハンドラの登録を解除
+                grid.SelectionChanged -= OnSelectionChanged;
+            }
+
             base.OnDetaching();
-            if (AssociatedObject != null) AssociatedObject.SelectionChanged -= OnSelectionChanged;
         }
 
-        private void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
-        {
-            if (SelectedItems == null) return;
-
-            if (e.AddedItems != null)
-            {
-                foreach (var item in e.AddedItems) SelectedItems.Add(item);
-            }
-
-            if (e.RemovedItems != null)
-            {
-                foreach (var item in e.RemovedItems) SelectedItems.Remove(item);
-            }
-        }
+        #region 選択されたアイテムリストの依存プロパティの登録
+        public static DependencyProperty SelectedItemsProperty =
+            DependencyProperty.Register("SelectedItems", typeof(IList), typeof(BindSelectedItemsBehavior),
+                //new PropertyMetadata(null));
+                new FrameworkPropertyMetadata(null, FrameworkPropertyMetadataOptions.BindsTwoWayByDefault, OnSelectedItemsChanged));
 
         private static void OnSelectedItemsChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
-            if (d is DataGridSelectedItemsBlendBehavior behavior)
+            if (d is BindSelectedItemsBehavior behavior)
             {
                 var dataGrid = behavior.AssociatedObject;
 
@@ -68,10 +72,32 @@ namespace fname2timestamp.Behaviors
             }
         }
 
-        public IList<object> SelectedItems
+        /// <summary>
+        /// 選択されたアイテムリスト
+        /// </summary>
+        public IList SelectedItems
         {
-            get { return (IList<object>)GetValue(SelectedItemsProperty); }
+            get { return (IList)GetValue(SelectedItemsProperty); }
             set { SetValue(SelectedItemsProperty, value); }
+        }
+        #endregion
+
+        /// <summary>
+        /// 選択されたアイテムリストを選択されたアイテムの変更に応じて更新
+        /// </summary>
+        void OnSelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            //新たに選択されたアイテムをリストに追加
+            foreach (var addedItem in e.AddedItems)
+            {
+                SelectedItems.Add(addedItem);
+            }
+
+            //選択解除されたアイテムをリストから削除
+            foreach (var removedItem in e.RemovedItems)
+            {
+                SelectedItems.Remove(removedItem);
+            }
         }
     }
 }
